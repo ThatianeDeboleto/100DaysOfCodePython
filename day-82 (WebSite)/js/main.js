c32d2aa6e912be86a2fed75047a08bd2cec59685
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2021-12-27 15:11:35 -0300

    ADD Projeto dia 82

---
+++ day-82 (WebSite)/js/main.js
@@ -0,0 +1,62 @@
+jQuery(document).ready(function ($) {
+  //FIXED HEADER
+  window.onscroll = function () {
+    if (window.pageYOffset > 140) {
+      $("#header").addClass("active");
+    } else {
+      $("#header").removeClass("active");
+    }
+  };
+
+  //ISOTOPE
+  let btns = $("#servicos .button-group button");
+
+  btns.click(function (e) {
+    $("#servicos .button-group button").removeClass("active");
+    e.target.classList.add("active");
+
+    let selector = $(e.target).attr("data-filter");
+    $("#servicos .grid").isotope({
+      filter: selector,
+    });
+  });
+
+  $(window).on("load", function () {
+    $("#servicos .grid").isotope({
+      filter: "*",
+    });
+  });
+
+  //MAGNIFY
+  $(".grid .popup-link").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true,
+      tPrev: "Anterior",
+      tNext: "Próxima",
+      tCounter: "%curr% de %total%",
+    },
+  });
+
+  //OWL
+  $(".owl-carousel").owlCarousel({
+    loop: false,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    dots: true,
+    lazyLoad: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+});
