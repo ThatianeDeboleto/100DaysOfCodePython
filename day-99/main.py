fb50cf520024404439fc21a9de336095278ed48a
Thatiane Deboleto <drathatianenimtzz@icloud.com>
2022-01-01 09:49:07 -0300

    ADD projeto 99

---
+++ day-99/main.py
@@ -0,0 +1,7 @@
+'''Projeto dia 99
+analise de mortes - pesquisa de policiais
+
+
+Extraia ideias da combinação de dados do censo dos EUA e do banco de dados do Washington Post sobre mortes por policiais 
+nos Estados Unidos.'''''
+
